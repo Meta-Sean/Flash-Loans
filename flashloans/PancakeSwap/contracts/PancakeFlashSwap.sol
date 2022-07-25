68c67ab45015fbe9bb781c4a4fed2a3683b9b2f4
Meta-Sean <seanhuckabee@gmail.com>
2022-07-25 15:46:35 -0500

    Initiate Arb

--- flashloans/PancakeSwap/contracts/PancakeFlashSwap.sol
+++ flashloans/PancakeSwap/contracts/PancakeFlashSwap.sol
@@ -8,5 +8,78 @@ import "./libraries/SafeERC20.sol";
 import "./interfaces/IUniswapV2Router01.sol";
 import "./interfaces/IUniswapV2Pair.sol";
 import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IERC20.sol";
 
-contract PancakeFlashSwap {}
+contract PancakeFlashSwap {
+    using SafeERC20 for IERC20;
+
+    // Factory and Routing Addresses
+    address private constant PANCAKE_FACTORY =
+        0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
+    address private constant PANCAKE_ROUTER =
+        0x10ED43C718714eb63d5aA57B78B54704E256024E;
+
+    // Token Addresses
+    address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address private constant BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
+    address private constant CAKE = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82;
+    address private constant CROX = 0x2c094F5A7D1146BB93850f629501eB749f6Ed491;
+
+    // Trade Variables
+    uint256 private deadline = block.timestamp + 1 days;
+    uint256 private constant MAX_INT =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
+    // Fund Smart Contract
+    // Provides Function to allow contract to be funded
+    function fundFlashSwapContrat(
+        address _owner,
+        address _token,
+        uint256 _amount
+    ) public {
+        IERC20(_token).transferFrom(_owner, to, address(this), _amount);
+    }
+
+    // Get Contract Balance
+    // Allows public view of balance
+    function getBalanceOfToken(address _address) public view returns (uint256) {
+        return IERC20(_address).balanceOf(address(this));
+    }
+
+    // Initiate Arbitrage
+    // Begins receiving loan and to engge performing arbitrage trades
+    function startArb(address _tokenBorrow, uint256 _amount) external {
+        IERC20(BUSD).safeApprove(address(PANCAKE_ROUTER), MAX_INT);
+        IERC20(USDT).safeApprove(address(PANCAKE_ROUTER), MAX_INT);
+        IERC20(CROX).safeApprove(address(PANCAKE_ROUTER), MAX_INT);
+        IERC20(CAKE).safeApprove(address(PANCAKE_ROUTER), MAX_INT);
+
+        // Get the Factory Pair address for combined tokens
+        address pair = IUniswapV2Factory(PANCAKE_FACTORY).getPair(
+            _tokenBorrow,
+            WBNB
+        );
+
+        // Return error if combination does not exist
+        require(pair != address(0), "Pool does not exist");
+
+        // Figure out which token (0 or 1) has the amount and assign
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+        uint256 amount0Out = _tokenBorrow == token0 ? _amount : 0;
+        uint256 amount1Out = _tokenBorrow == token1 ? _amount : 0;
+
+        // Passing data as bytes so that the 'swap function knows it is a flashloan
+        bytes memory data = abi.encode(_tokenBorrow, _amount);
+
+        // Execute the initial swap to get the loan
+        IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), data);
+    }
+
+    function pancakeCall(
+        address _sender,
+        uint256 _amount0,
+        uint256 _amount1,
+        bytes calldata _data
+    ) external {}
+}
